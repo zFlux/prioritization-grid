20216b11bfbaee60f40e744b3ab1edb86ded40d2
Daniel Christo <daniel.c@thinkific.com>
2023-02-23 23:24:05 -0500

    refactor(utils): Refactor functions into utils

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { replaceAt } from './utils';
+import { replaceAt, convertToItemNumbersInRankedOrder } from './utils';
 
 describe('replaceAt', () => {
     it('replaces at the beginning', () => {
@@ -11,5 +11,23 @@ describe('replaceAt', () => {
     
     it('replaces in the middle', () => {
         expect(replaceAt([1, 2, 3], 1, 0)).toEqual([1, 0, 3]);
+    });
+});
+
+describe('convertToItemNumbersInRankedOrder', () => {
+    it('leaves item number is original order when list of item counts are all zero', () => {
+        expect(convertToItemNumbersInRankedOrder([0,0,0,0,0,0,0,0,0,0])).toEqual([0,1,2,3,4,5,6,7,8,9]);
+    });
+
+    it('elevates item number to first place when it has the highest count', () => {
+        expect(convertToItemNumbersInRankedOrder([0,0,0,0,0,0,0,0,0,1])).toEqual([0,9,1,2,3,4,5,6,7,8]);
+    });
+
+    it('leaves item numbers in original order when counts are all equal', () => {
+        expect(convertToItemNumbersInRankedOrder([1,1,1,1,1,1,1,1,1,1])).toEqual([0,1,2,3,4,5,6,7,8,9]);
+    });
+
+    it('orders item numbers by count', () => {
+        expect(convertToItemNumbersInRankedOrder([0,0,0,0,0,0,0,0,2,1])).toEqual([0,8,9,1,2,3,4,5,6,7]);
     });
 });
